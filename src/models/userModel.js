99e878a6e3bffc03c0a6f7eb87f1aca603ee6f0c
Joe Preziuso <jpreziuso14@gmail.com>
2024-08-16 10:21:59 -0400

    Add changes to all files

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,16 +1,17 @@
-//javascript
-// User.js
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  // Add more fields as needed
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
